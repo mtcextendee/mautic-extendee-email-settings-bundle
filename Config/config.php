68869d9d18f78558c4f9c800dbfa09174fd9b8e5
Zdeno Kuzmany <zdeno@kuzmany.biz>
2018-11-08 10:24:23 +0100

    Init commit

---
+++ Config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'name'        => 'MauticExtendeeEmailSettingBundle',
+    'description' => '',
+    'author'      => 'kuzmany.biz',
+    'version'     => '1.0.0',
+];
---
+++ MauticExtendeeEmailSettingBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MauticPlugin\MauticExtendeeEmailSettingBundle;
+
+use Mautic\PluginBundle\Bundle\PluginBundleBase;
+
+class MauticExtendeeEmailSettingBundle extends PluginBundleBase
+{
+}
